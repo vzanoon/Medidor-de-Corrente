56dfb8404405bbbd4fb2727cb09b1cf27b75cb4a
Vinícius Zanon <36170895+vzanoon@users.noreply.github.com>
2019-07-04 21:20:22 -0300

    Configurando Sinal de Referência do Arduino e Update in Photos

--- projeto_final/projeto_final.ino
+++ projeto_final/projeto_final.ino
@@ -1,32 +1,32 @@
 #define PINA0 0
-
 int tensao = 220;
 float potencia = 0;
-float voltsporUnidade = 0.004887586;// 5%1023 //parâmetros para calibração do sensor
+float voltsporUnidade = 0.0039100684;// 4%1023 //parâmetros para calibração do sensor
 int sensorValue_aux  = 0;
 float sensorValue = 0;
 float currentValue = 0;
 int samples = 1500;
-float Vref = 2.15;
-int ref = Vref/voltsporUnidade;
+float Vref = 2.18;
+int ref = (Vref/voltsporUnidade);
 int gain = 5;
 float max_current = 10;
 float Vin = 0.300;
-float sensibility = ((Vin*gain)/sqrt(2))/max_current;
+float sensibility = (((Vin*gain)/sqrt(2))/(max_current));
 
 void setup() {
   Serial.begin(115200);
   pinMode(PINA0,INPUT);
+  analogReference(EXTERNAL);
 }
 
 void loop() {
   
   for(int i=samples; i>0; i--){
-    sensorValue_aux = (analogRead(PINA0) - ref); // le o sensor na pino analogico A0 e ajusta o valor lido ja que a saída do sensor é (1023)vcc/2 para corrente = 0
+    sensorValue_aux = (analogRead(PINA0) - ref); // le o sensor no pino analogico A0 e ajusta o valor lido ja que a saída do sensor é (1023)vcc/2 para corrente = 0
     sensorValue += pow(sensorValue_aux,2); // somam os quadrados das leituras.
   }
   
-  sensorValue = (sqrt(sensorValue/samples)) * voltsporUnidade; // finaliza o calculo da méida quadratica e ajusta o valor lido para volts
+  sensorValue = (sqrt(sensorValue/samples)) * voltsporUnidade; // finaliza o calculo da média quadratica e ajusta o valor lido para volts
   currentValue = (sensorValue/sensibility); // calcula a corrente considerando a sensibilidade do sensor
   potencia = currentValue*tensao;
   
@@ -34,8 +34,9 @@ void loop() {
   Serial.println(analogRead(PINA0));
   Serial.print("SENSOR:  ");
   Serial.println(sensorValue);
-  Serial.print("CORRENTE:  ");
+  Serial.print("CORRENTE(A):  ");
   Serial.println(currentValue);
-  Serial.print("POTENCIA:  ");
+  Serial.print("POTENCIA(W):  ");
   Serial.println(potencia);
+  
 }
